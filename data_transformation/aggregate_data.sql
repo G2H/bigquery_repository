056829ab22a68d7f1676557c23b39996e5d90720
Guillaume-Henri Huon <ghh@google.com>
2025-03-19 11:17:03 +0000

    edited column name

--- data_transformation/aggregate_data.sql
+++ data_transformation/aggregate_data.sql
@@ -4,7 +4,7 @@ SELECT
   product_name,
   EXTRACT(YEAR FROM order_date) AS sale_year,
   EXTRACT(MONTH FROM order_date) AS sale_month,
-  SUM(total_amount) AS total_sales
+  SUM(total_amount) AS revenue_usd
 FROM mydataset.ingested_data
   
 GROUP BY 1,2,3;
